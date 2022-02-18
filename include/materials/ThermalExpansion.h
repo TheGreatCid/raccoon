f9529728a091f20eadc45d879d2362c3cab1cbd7
TheGreatCid <david.torres@duke.edu>
2022-02-18 17:02:18 -0500

    added gary's texp classes

---
+++ include/materials/ThermalExpansion.h
@@ -0,0 +1,40 @@
+//* This file is part of the RACCOON application
+//* being developed at Dolbow lab at Duke University
+//* http://dolbow.pratt.duke.edu
+
+#pragma once
+
+#include "Material.h"
+#include "BaseNameInterface.h"
+#include "ADRankTwoTensorForward.h"
+#include "Function.h"
+
+/**
+ * This class computes the deformation gradient
+ */
+class ThermalExpansion : public Material, public BaseNameInterface
+{
+public:
+  static InputParameters validParams();
+
+  ThermalExpansion(const InputParameters & parameters);
+
+protected:
+  virtual void initQpStatefulProperties() override;
+
+  virtual void computeQpProperties() override;
+
+  // The thermal eigen deformation gradient
+  ADMaterialProperty<RankTwoTensor> & _Fg;
+  const MaterialProperty<RankTwoTensor> & _Fg_old;
+
+  // The thermal expansion coefficient
+  const Function & _alpha;
+
+  // The current temperature
+  const ADVariableValue & _T;
+  const VariableValue & _T_old;
+
+  // The reference temperature
+  const VariableValue & _T0;
+};
---
+++ src/materials/ThermalExpansion.C
@@ -0,0 +1,62 @@
+//* This file is part of the RACCOON application
+//* being developed at Dolbow lab at Duke University
+//* http://dolbow.pratt.duke.edu
+
+#include "ThermalExpansion.h"
+
+registerADMooseObject("raccoonApp", ThermalExpansion);
+
+InputParameters
+ThermalExpansion::validParams()
+{
+  InputParameters params = Material::validParams();
+  params += BaseNameInterface::validParams();
+  params.addClassDescription(
+      "This class computes the thermal deformation gradient, $\\bfF = (1+\\alpha(T-T_0))\bfI$, "
+      "where \\alpha is the thermal expansion coefficient, $T$ is the temperature, and $T_0$ is "
+      "the reference temperature corresponding to zero thermal expansion.");
+
+  params.addRequiredParam<MaterialPropertyName>("eigen_deformation_gradient_name",
+                                                "The name of the thermal deformation gradient");
+  params.addRequiredParam<FunctionName>(
+      "thermal_expansion_function",
+      "Function describing the thermal expansion coefficient $\alpha$");
+  params.addRequiredCoupledVar("temperature", "The current temperature");
+  params.addRequiredCoupledVar("reference_temperature",
+                               "The reference temperature corresponding to zero thermal expansion");
+
+  params.suppressParameter<bool>("use_displaced_mesh");
+  return params;
+}
+
+ThermalExpansion::ThermalExpansion(const InputParameters & parameters)
+  : Material(parameters),
+    BaseNameInterface(parameters),
+    _Fg(declareADProperty<RankTwoTensor>(prependBaseName("eigen_deformation_gradient_name", true))),
+    _Fg_old(getMaterialPropertyOldByName<RankTwoTensor>(
+        prependBaseName("eigen_deformation_gradient_name", true))),
+    _alpha(getFunction("thermal_expansion_function")),
+    _T(adCoupledValue("temperature")),
+    _T_old(coupledValueOld("temperature")),
+    _T0(coupledValue("reference_temperature"))
+{
+}
+
+void
+ThermalExpansion::initQpStatefulProperties()
+{
+  _Fg[_qp].setToIdentity();
+}
+
+void
+ThermalExpansion::computeQpProperties()
+{
+  const Real old_temp = _t_step <= 1 ? _T0[_qp] : _T_old[_qp];
+
+  const Point p;
+  const Real alpha = _alpha.value(MetaPhysicL::raw_value(_T[_qp]), p);
+  const Real alpha_old = _alpha.value(old_temp, p);
+  const Real alpha_avg = (alpha + alpha_old) / 2;
+
+  _Fg[_qp] = (1 + alpha_avg * (_T[_qp] - old_temp)) * _Fg_old[_qp];
+}
