165a8c20de53f8a34021a4b3fc260de75f51c1db
TheGreatCid <davidt.realmadrid@gmail.com>
2023-12-14 12:27:40 -0500

    added recover stress divergence tensors

---
+++ include/kernels/ADDynamicStressDivergenceTensorsRecover.h
@@ -0,0 +1,42 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+
+#pragma once
+
+#include "ADStressDivergenceTensors.h"
+
+/**
+ * ADDynamicStressDivergenceTensorsRecover is the automatic
+ * differentiation version of DynamicStressDivergenceTensors.
+ * This kernel derives from ADStressDivergenceTensors and
+ * adds stress related Rayleigh and HHT time integration terms.
+ */
+class ADDynamicStressDivergenceTensorsRecover : public ADStressDivergenceTensors
+{
+public:
+  static InputParameters validParams();
+
+  ADDynamicStressDivergenceTensorsRecover(const InputParameters & parameters);
+
+protected:
+  ADReal computeQpResidual();
+
+  ///{@ The old and older states of the stress tensor that the divergence operator operates on
+  const MaterialProperty<RankTwoTensor> & _stress_older;
+  const MaterialProperty<RankTwoTensor> & _stress_old;
+  ///@}
+
+  // Rayleigh damping parameter _zeta and HHT time integration parameter _alpha
+  const MaterialProperty<Real> & _zeta;
+  const Real _alpha;
+  const bool _static_initialization;
+
+  const ADMaterialProperty<RankTwoTensor> & _stress_old_store;
+  const ADMaterialProperty<RankTwoTensor> & _stress_older_store;
+};
